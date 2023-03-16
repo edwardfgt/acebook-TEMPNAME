52e7cabee83bc36d1e897883115484ef57b2eb0d
Max Allan-Smith <Maxibon_1195@hotmail.com>
2023-03-16 12:14:26 +0000

    Updated Profile Page

--- frontend/src/components/profile/Profile.js
+++ frontend/src/components/profile/Profile.js
@@ -1,41 +1,66 @@
-import * as React from 'react'
-import { useParams } from 'react-router-dom'
-import { useEffect } from 'react'
-
-const Profile = ({ navigate }) => {
-  const [user, setUser] = React.useState()
-  const { id } = useParams()
-
-useEffect(() => {
-  getUserDetails()
-}, [])
-
-
-  const getUserDetails = async () => {
-    const response = await fetch(`/users/userInfo/${id}`);
-    const json = await response.json();
-  
-
-    if(response.ok) {
-      console.log(response)
-      setUser(json);
-    }
-  }
-  
-
+import React, { Component } from 'react';
+import '../profile/Profile.css'
+class ProfilePage extends Component {
+  state = {}
+  render() {
     return (
       <>
-      {user ?
-      <>
-      <h1>{user.user.firstName} {user.user.lastName}</h1>
-      <h2>{user.user.email}</h2> 
+        <div class="profilePageContainer">
+          <div className="coverPhoto">
+            <img src='/Test-Cover-Photo.jpeg'></img>
+          </div>
+          <div className="userProfileDetails">
+            <div className='innerProfile'>
+              <div className='profilePicture'>
+                {/* 🔽 This needs to be made dynamic so that the user can change it. */}
+                <img src='/Test-Profile-Picture.jpeg'></img>
+              </div>
+              <div className='userFullName'>
+                <h2>Max Allan-Smith</h2>
+              </div>
+              <div className='friendCount'>
+                <h5>551 Friends</h5>
+              </div>
+              <div className='buttonContainer'>
+                <div className='btn btn-outline-primary'>
+                  <a>Edit Profile</a>
+                </div>
+              </div>
+            </div>
+            <div id="profilePageBreak" className="lineBreak"></div>
+            <nav id="profileNavBar" class="navbar navbar-expand-lg navbar-light">
+              <a class="navbar-brand" href="#">Posts ▾</a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                  <li class="nav-item active">
+                    <a class="nav-link" href="#">About<span class="sr-only"></span></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Friends</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Photos</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Videos</a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+          </div>
+        </div>
+        <div id="feedContainer " className='container'>
+          <div className='feed'>
+          </div>
+        </div>
       </>
-      :
-      <h1>Can't find user</h1>
-    }
-      </>
-    )
-
+    );
+  }
 }
 
-export default Profile
+
+
+export default ProfilePage;
